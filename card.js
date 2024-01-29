fb9373f29655c779acb4b3ca1c3547591e47759e
Antonio Contreras <antoniocontreras71@yahoo.com>
2024-01-29 11:33:14 -0600

    incorporating OOP

--- card.js
+++ card.js
@@ -1,6 +1,22 @@
-// JavaScript source code
+//all Cards class
 class Card {
-  constructor(type) {
+  constructor(name, color, set, type, playCost) {
+    this.name = "Agumon";
+    this.color =  "Red";
+    this.set = set;
     this.type = type;
+    this.playCost = playCost;
   }
 }
+//inheriting parent class
+class Digimon extends Card {
+  constructor(name, color, set, playCost, level) {
+    //call in the super class constructor and pass in the name variable
+    super(name, color, set, playCost);
+    //overriding the name
+    this.playCost = playCost;
+  }
+  
+}
+let agumon = new Digimon("Agumon", "Red", "BT-001", 3, 6);
+alert(agumon.playCost);
